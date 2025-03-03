0d3962275aebd1a144b89b4acb989ceef665dc0b
Your Name <your-email@example.com>
2025-03-03 09:46:29 +0100

    Updated files

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -4,9 +4,10 @@ const Footer = () => {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer 
-    className="footer  bg-gray-900 text-white text-center flex justify-center items-center">
+    <footer
+    className="footer  bg-gray-900 text-white text-center flex justify-center items-center"
          style={{ height: "8vh" }}
+         >
       <p>&copy; {currentYear} My Website. All Rights Reserved.</p>
     </footer> 
   );
